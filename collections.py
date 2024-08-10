4ca87236f01eb0164d0d66ae7ff1b455920a6339
prasad <peruguprasad0@gmail.com>
2024-08-10 11:19:10 +0530

    collections added

---
+++ collections.py
@@ -0,0 +1,14 @@
+# collections - a collection of values
+# list - a collection of values in a specific order
+list = [1,2,3,'a']
+print(list)
+# tuple - a collection of values in a specific order, but immutable
+tuple = (1,2,3,'a')
+print(tuple)
+# set - a collection of values unordered and does not allow duplicates
+set = {1,2,3}
+print(set)
+{1,2,2,2,'a'} # this is the same as {1,2,'a'}
+# dictionary - a set of unordered key, value pairs
+dic = {1:'a', 2:'b', 3:'c'}
+print(dic[1])
